dea8ab38d4ab8bbc190cd46b23b8173980b49431
h.chobanov <hristiqn_chobanov@abv.bg>
2022-12-11 18:29:09 +0200

    1231232

--- Resources/views/admin/breadcrumbs.blade.php
+++ Resources/views/admin/breadcrumbs.blade.php
@@ -1,22 +1,23 @@
 <div class="breadcrumbs">
     <ul>
-        <li>
-            <a href="{{ route('admin.index') }}"><i class="fa fa-home"></i></a>
-            <i class="fa fa-angle-right"></i>
-        </li>
-        <li>
-            <a href="{{ route('ad-boxes') }}" class="text-black">@lang('adboxes::admin.adboxes.index')</a>
-        </li>
-        @if(url()->current() === route('ad-boxes.create'))
+        @if(url()->current() === route('ad-boxes'))
+            <li>
+                <a href="{{ route('admin.index') }}"><i class="fa fa-home"></i></a>
+                <i class="fa fa-angle-right"></i>
+            </li>
+            <li>
+                <a href="{{ route('ad-boxes') }}" class="text-black">@lang('adboxes::admin.adboxes.index')</a>
+            </li>
+        @elseif(url()->current() === route('ad-boxes.create'))
             <li>
                 <i class="fa fa-angle-right"></i>
                 <a href="{{ route('ad-boxes.create') }}" class="text-purple">@lang('adboxes::admin.adboxes.create')</a>
             </li>
-{{--        @elseif(url()->current() === route('ad-boxes.edit'))--}}
-{{--            <li>--}}
-{{--                <i class="fa fa-angle-right"></i>--}}
-{{--                <a href="{{ route('ad-boxes.edit') }}" class="text-purple">@lang('adboxes::admin.adboxes.edit')</a>--}}
-{{--            </li>--}}
+        @elseif(url()->current() === route('ad-boxes.edit', ['id' => Request::segment(3)]))
+            <li>
+                <i class="fa fa-angle-right"></i>
+                <a href="{{ route('ad-boxes.edit', ['id' => Request::segment(3)]) }}" class="text-purple">@lang('adboxes::admin.adboxes.edit')</a>
+            </li>
         @endif
     </ul>
 </div>
